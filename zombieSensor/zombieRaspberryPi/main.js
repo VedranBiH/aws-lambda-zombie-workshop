e50d5af3749ba1d50bcd1b71e45a1e302683359d
Sam Nunnally <samuel_nunnally@csx.com>
2017-07-23 14:08:14 -0400

    changes for CSX and publish to mult regions

--- zombieSensor/zombieRaspberryPi/main.js
+++ zombieSensor/zombieRaspberryPi/main.js
@@ -1,32 +1,22 @@
 var rpio = require('rpio');
 var AWS = require('aws-sdk'); //require AWS SDK
 // For variables that ask for region input, please use region code such as us-west-2 or us-east-1
-AWS.config.update({accessKeyId: 'ENTER ACCESS KEY HERE', secretAccessKey: 'ENTER SECRET ACCESS KEY HERE', region: 'ENTER REGION HERE'}); //configure AWS credentials
-var sns = new AWS.SNS({region: 'ENTER REGION HERE'}); // establish the SNS connection
+AWS.config.update({ accessKeyId: '', secretAccessKey: '', region: 'us-east-1' }); //configure AWS credentials
+var snsEast1 = new AWS.SNS({ region: 'us-east-1' }); // establish the SNS connection
+var snsEast2 = new AWS.SNS({ region: 'us-east-2' });
+var snsWest1 = new AWS.SNS({ region: 'us-west-1' });
+var snsWest2 = new AWS.SNS({ region: 'us-west-2' });
 
 var zombieSensor = rpio.open(11, rpio.INPUT, rpio.PULL_DOWN); //setup digital read on Digital pin #11
-var led = rpio.open(3, rpio.OUTPUT, rpio.LOW);
+//var led = rpio.open(3, rpio.OUTPUT, rpio.LOW);
 rpio.poll(11, periodicActivity);
 
-var city = 
-  [ 
-    ["London",51.507351,-0.127758],
-    ["Las Vegas",36.169941,-115.139830],
-    ["New York",40.712784,-74.005941],
-    ["Singapore",1.352083,103.819836],
-    ["Sydney",-33.867487,151.206990],
-    ["Paris",48.856614,2.352222],
-    ["Seattle",47.606209,-122.332071],
-    ["San Francisco",37.774929,-122.419416],
-    ["Montreal",45.501689,-73.567256],
-    ["Rio De Janeiro",-22.906847,-43.172896],
-    ["Beijing",39.904211,116.407395],
-    ["Moscow",55.755826,37.617300],
-    ["Buenos Aires",-34.603684,-58.381559],
-    ["New Dehli",28.613939,77.209021],
-    ["Cape Town",-33.924869,18.424055],
-    ["Lagos",6.524379,3.379206],
-    ["Munich",48.135125,11.581981]
+var location =
+  [
+    ["Southpoint Building 1", 30.253333, -81.58907],
+    ["Southpoint Building 2", 30.252738, -81.590432],
+    ["550 Building 550 Water St.", 30.325798, -81.6650521],
+    ["CSX HQ 500 Water St.", 30.324513, -81.664107],
   ]
 
 
@@ -37,40 +27,51 @@ var city =
 //Function to generate a randomNumber to be used for selecting random cities
 function roundedNumberFunc(callback) {
 
-  var randomNumber =  Math.random() * (15 - 0 + 1) + 0;
+  var randomNumber = Math.random() * (3 - 0) + 0;
   var roundedNumber = Math.round(randomNumber)
-  generateAlert(roundedNumber, city) //call the function to send the alert to SNS
+  generateAlert(roundedNumber, location) //call the function to send the alert to SNS
 
 }
 
 // function to form the message with a random city and send it to SNS
-function generateAlert(roundedNumber, city) {
-
-  var message = '{"message":"A Zombie has been detected in ' + city[roundedNumber][0] + '!", "value":"1", "city":"' + city[roundedNumber][0] + '", "longitude":"' + city[roundedNumber][2] + '", "latitude":"' + city[roundedNumber][1] + '"}'
+function generateAlert(roundedNumber, location) {
+  console.log("roundedNumber="+roundedNumber)
+  var message = '{"message":"A Zombie has been detected in ' + location[roundedNumber][0] + '!", "value":"1", "city":"' + location[roundedNumber][0] + '", "longitude":"' + location[roundedNumber][2] + '", "latitude":"' + location[roundedNumber][1] + '"}'
   var params = {
     Message: message, /* required */
     Subject: "Zombie Alert",
-    TopicArn: "ENTER YOUR TOPIC ARN HERE"
+    TopicArn: ""
   };
-  sns.publish(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data + " Message successfully sent to SNS");           // successful response
-  });
+  publish(params, snsEast1);
 
-  console.log("Zombie Detected") 
+  params.TopicArn = "";
+  publish(params, snsEast2);
+
+  params.TopicArn = "";
+  publish(params, snsWest1);
+
+  params.TopicArn = "";
+  publish(params, snsWest2);
+
+  console.log("Zombie Detected")
+}
+
+function publish(params, sns) {
+  sns.publish(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data + " Message successfully sent to SNS");           // successful response
+  });
 }
 
 // Period function to run every second and read the sensor vlaue.
 function periodicActivity(pin) {
   var sensorValue = rpio.read(pin);
   console.log("sensorValue = " + sensorValue);
-    if (sensorValue == 1) { 
-      roundedNumberFunc() // Sensor value is 1 (motion detected), start function to generate random number and publish message to SNS
-      rpio.write(3, rpio.HIGH);
-    }
-    else {
-      console.log("The Coast is Clear") //No motion detected
-      rpio.write(3, rpio.LOW);
-    };
+  if (sensorValue == 1) {
+    roundedNumberFunc() // Sensor value is 1 (motion detected), start function to generate random number and publish message to SNS
+  }
+  else {
+    console.log("The Coast is Clear") //No motion detected
+  };
 }
 
